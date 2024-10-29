705de8bda4408e21e8557290320ff92eb635e0c2
MatheusHrique <notch02005@gmail.com>
2024-10-28 21:15:45 -0300

    adding correct anwsers & backend code

--- js/script.js
+++ js/script.js
@@ -1,16 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
     function getCheckedValues(event) {
         event.preventDefault();
-
-        const checkboxes = document.querySelectorAll('input[type="checkbox"]:checked');
+        ;
+        const checkboxes = document.querySelectorAll('input[type="checkbox"]:checked')
         const selectedValues = Array.from(checkboxes).map(checkbox => checkbox.value);
-
-        alert('You selected: ' + selectedValues.join(', '));
     }
 
     // const sintomasSelecionados = document.querySelectorAll('input[name="sintomas"]:checked');
+    let totalScore = 0;
     const Score = {
-        totalScore: 0,
         q1: 0,
         q2: 0,
         q3: 0,
@@ -23,21 +21,26 @@ document.addEventListener('DOMContentLoaded', function () {
         q10: 0
     };
 
-    sintomasSelecionados.forEach((sintoma) => {
-        // Atualize a pontuação com base na alternativa selecionada
+    Score.forEach((awnser) => {
+        if (selectedValues[1] === 'Pamela Vorhess') Score.q1 = 1;
+        if (selectedValues[2] === 'All work and no play makes Jack a dull boy') Score.q2 = 1;
+        if (selectedValues[3] === 'The Conjuring') Score.q3 = 1;
+        if (selectedValues[4] === 'Poltergeists') Score.q4 = 1;
+        if (selectedValues[5] === 'Freddy Krueger') Score.q5 = 1;
+        if (selectedValues[6] === 'A forest') Score.q6 = 1;
+        if (selectedValues[7] === 'The Ghostface mask') Score.q7 = 1;
+        if (selectedValues[8] === 'The Ring') Score.q8 = 1;
+        if (selectedValues[9] === 'Clowns') Score.q9 = 1;
+        if (selectedValues[10] === 'Brain surgery') Score.q10 = 1;
+    })
 
-        if (selectedValues[1] === 'a1') Score.q1 += 1;
-        if (selectedValues[2] === 'a2') Score.q2 += 1;
-        if (selectedValues[3] === 'a3') Score.q3 += 1;
-        if (selectedValues[4] === 'a4') Score.q4 += 1;
-        if (selectedValues[5] === 'a5') Score.q5 += 1;
-        if (selectedValues[6] === 'a6') Score.q6 += 1;
-        if (selectedValues[7] === 'a7') Score.q7 += 1;
-        if (selectedValues[8] === 'a8') Score.q8 += 1;
-        if (selectedValues[9] === 'a9') Score.q9 += 1;
-        if (selectedValues[10] === 'a10') Score.q10 += 1;
+    for (let i = 1; i != 10; i++) {
+        
+    }
 
-    });
+    // sintomasSelecionados.forEach((sintoma) => {
+    // Atualize a pontuação com base na alternativa selecionada
+    // });
 
     // document.getElementById('resultado').innerHTML = `
     //     <ul>
